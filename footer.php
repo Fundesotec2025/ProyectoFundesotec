651cce7a647191477edadbbf78e6e4d2eac14086
Porfirio Jimenez <fundesotec.osfl@fundesotec.onmicrosoft.com>
2025-05-07 15:34:06 -0500

    CAMBIOS REALIZADOS EL 7

--- footer.php
+++ footer.php
@@ -9,27 +9,27 @@
             <h3 class="text-center font-weight-bold text-white mb-4">Nuestras redes sociales</h3>
 
             <!-- Redes Sociales -->
-            <div class="col-2 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
+            <div class="col-3 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
                 <a href="https://www.facebook.com/fundesotec" target="_blank" aria-label="Facebook de Fundesotec">
                     <i class="bi bi-facebook icono-redes"></i>
                 </a>
             </div>
-            <div class="col-2 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
+            <div class="col-3 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
                 <a href="https://www.instagram.com/fundesotec" target="_blank" aria-label="Instagram de Fundesotec">
                     <i class="bi bi-instagram icono-redes"></i>
                 </a>
             </div>
-            <div class="col-2 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
+            <div class="col-3 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
                 <a href="https://www.tiktok.com/@fundesotec" target="_blank" aria-label="TikTok de Fundesotec">
                     <i class="bi bi-tiktok icono-redes"></i>
                 </a>
             </div>
-            <div class="col-2 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
+            <div class="col-3 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
                 <a href="https://www.linkedin.com/company/fundación-desarrollo-social-y-tecnológico" target="_blank" aria-label="LinkedIn de Fundesotec">
                     <i class="bi bi-linkedin icono-redes"></i>
                 </a>
             </div>
-            <div class="col-2 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
+            <div class="col-3 col-md-1 d-flex justify-content-center align-items-center redsocial1 mb-3">
                 <a href="https://www.youtube.com/@fundesotec" target="_blank" aria-label="YouTube de Fundesotec">
                     <i class="bi bi-youtube icono-redes"></i>
                 </a>
@@ -44,10 +44,13 @@
             <div class="col-12 col-md-3 contDatos mb-4 mb-md-0">
                 <h4 class="datosContactos">DIRECCIÓN</h4><br>
                 <p class="informacion" itemscope itemtype="https://schema.org/PostalAddress">
-                    <i class="bi bi-geo-alt"></i>
-                    <strong>País: Ecuador</strong><br>
-                    <a href="https://www.google.com/maps/search/?api=1&query=Juan+Carlos+Lavalle+N9-461,+Quito,+Ecuador" target="_blank">
-                        Calle, Juan Carlos Lavalle N9-461, Quito 170803
+                    <div style="display: inline-flex; align-items: center;">
+                        <i class="bi bi-geo-alt" style="margin-right: 8px;"></i>
+                        <p class="fw-bold" style="margin-bottom: 0;">País: Ecuador</p>
+                    </div>
+
+                    <a href="https://www.google.com/maps/search/?api=1&query=Juan+Lavalle+N9-461,+Quito,+Ecuador" target="_blank">
+                        Calle, Juan Lavalle N9-461, Quito 170803
                     </a>
                 </p>
 
@@ -93,8 +96,9 @@
                 <h4 class="datosContactos text-center mb-3" style="font-size: 1.5rem; color: #9f07ba; font-weight: bold;">
                     <i class="bi bi-clock"></i> HORARIO DE ATENCIÓN
                 </h4>
+
                 <div class="card" style="background-color: #f8f9fa; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-                    <div class="card-body">
+                    <div style="text-align: center; padding: 1rem;">
                         <p class="informacion" style="font-size: 1.1rem; font-weight: bold; color: #333;">
                             <i class="bi bi-calendar-week" style="color: #007bff;"></i> LUNES A VIERNES
                         </p>
@@ -102,6 +106,13 @@
                             De 8:00 a. m. a 4:30 p. m.
                         </p>
                     </div>
+                </div>
+
+                <!-- Botón de Políticas de Privacidad con espaciado superior -->
+                <div class="text-center mt-4 mb-3">
+                    <a href="index.php?page=Vista/Politicas.html"  class="btn-donacion">
+                        Políticas de Privacidad
+                    </a>
                 </div>
             </div>
         </div>
@@ -112,24 +123,14 @@
         <!-- Derechos -->
         <div class="row">
             <div class="col text-center">
-                <h2 class="politicas">© 2018 - 2025 <span>Fundesotec</span>. Todos los derechos reservados</h2>
+                <h2>© 2018 - 2025 
+                    <span class="fundesotec-text">
+                        <span class="blue">FUND</span><span class="magenta">E</span><span class="blue">SOTEC</span>
+                    </span>. Todos los derechos reservados
+                </h2>
             </div>
         </div>
     </div>
- <!-- Barra de navegación tipo Footer -->
-<div class="secondary-footer brand-bg darken-2 text-center">
-    <div class="container">
-        <ul class="navbar-nav-footer">
-            <li><a class="nav-link-footer first-link" href="#top" class="page-scroll" data-section="#top">Inicio</a></li>
-            <li><a class="nav-link-footer" href="#nosotros" class="page-scroll" data-section="#nosotros">Nosotros</a></li>
-            <li><a class="nav-link-footer" href="#proyectos" class="page-scroll" data-section="#proyectos">Proyectos</a></li>
-            <li><a class="nav-link-footer"  href="index.php?page=Vista/Politicas.html"target="_blank">Políticas de privacidad</a></li>
-        </ul>
-    </div><!-- /.container -->
-</div><!-- /.secondary-footer -->
-</footer>
-
-
 </section>
 
 <!-- Scripts de Bootstrap -->
--- header.php
+++ header.php
@@ -6,77 +6,95 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fundesotec</title>
 
+    <!-- --------ICON FUNDESOTEC------------ -->
+    <link rel="manifest" href="manifest.json">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <!-- Google Fonts y Bootstrap Icons -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <!-- google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-    <!-- -------------------- -->
-    <!-- script correo -->
-    <script type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
 
+    <!-- EmailJS -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
     <script type="text/javascript">
-        emailjs.init('a8kALHJN1zzwKpep0')
+        emailjs.init('a8kALHJN1zzwKpep0');
     </script>
+
     <!-- Estilos personalizados -->
     <link rel="stylesheet" href="EstilosCss/Index.css">
+
+    <!-- Estilo extra para navbar scrolled -->
+    <style>
+        .navbar.scrolled {
+            background-color: #fff !important;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            transition: background-color 0.3s ease;
+        }
+
+        .navbar-brand img {
+            max-height: 50px;
+        }
+    </style>
 </head>
 
 <body>
 
- 
     <!-- Barra de navegación -->
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="Img/LogoFundesotec.png" alt="Logo Fundesotec"></a>
+            <a class="navbar-brand" href="index.php?page=Vista/Inicio.html">
+            <img src="Img/FUNDESOTEC.png" alt="Logo Fundesotec" class="img-fluid" id="logo">
+
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Inicio.html">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Nosotros.html">Nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Proyectos.html">Proyectos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Donaciones.html">Donaciones</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Noticias.html">Noticias</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Estructura.html">Estructura</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/CACFundesotec.html">CAC. Fundesotec</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Contactos.html">Contactos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Inicio.html" onclick="cerrarMenu()">INICIO</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Nosotros.html" onclick="cerrarMenu()">NOSOSTROS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Proyectos.html" onclick="cerrarMenu()">PROYECTOS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Donaciones.html" onclick="cerrarMenu()">DONACIONES</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Noticias.html" onclick="cerrarMenu()">NOTICIAS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Estructura.html" onclick="cerrarMenu()">ESTRUCTURA</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/CACFundesotec.html" onclick="cerrarMenu()">CAC. FUNDESOTEC</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?page=Vista/Contactos.html" onclick="cerrarMenu()">CONTACTOS</a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <!-- Script para cerrar el menú en móviles -->
+    <!-- Script para mejorar navegación responsiva -->
     <script>
         function cerrarMenu() {
             let navbarToggler = document.querySelector('.navbar-toggler');
             let navbarCollapse = document.querySelector('.navbar-collapse');
 
             if (navbarToggler && navbarCollapse.classList.contains('show')) {
-                navbarToggler.click();
+                navbarToggler.click(); // Cierra el menú
             }
-            return false;
         }
 
-    window.addEventListener('scroll', function () {
-        const navbar = document.querySelector('.navbar');
-        if (window.scrollY > 50) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
-</script>
+        window.addEventListener('scroll', function () {
+            const navbar = document.querySelector('.navbar');
+            if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        });
+    </script>
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-GYFjcBsIWpB8jR+aK+UJppNAm0SdbONs0Fbi2HG8p06FzncQ3nSOVv+yjCV5KfEe" crossorigin="anonymous"></script>
 
 </body>
 
